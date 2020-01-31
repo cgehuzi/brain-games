e4a41c8cea1550da1afe8b0222b95aef056bd232
cgehuzi <mail.cgehuzi@gmail.com>
2020-01-31 22:47:17 +0300

    [ menu ] - refactoring module logic

--- src/start.js
+++ src/start.js
@@ -7,27 +7,35 @@ import startGCD from './games/gcd';
 import startProgression from './games/progression';
 import startPrime from './games/prime';
 
-const gameDesc = 'You can play on one of this games:\n1 – brain-even\n2 – brain-calc\n3 — brain-gcd\n4 – brain-progression\n5 – brain-prime';
 const games = [
-  cons(1, startEven),
-  cons(2, startCalc),
-  cons(3, startGCD),
-  cons(4, startProgression),
-  cons(5, startPrime),
+  cons('brain-even', startEven),
+  cons('brain-calc', startCalc),
+  cons('brain-gcd', startGCD),
+  cons('brain-progression', startProgression),
+  cons('brain-prime', startPrime),
 ];
+
+const makeGameNav = (gamesList) => {
+  const result = ['You can play on one of this games:\n'];
+  for (let i = 0; i < gamesList.length; i += 1) {
+    const game = gamesList[i];
+    const [gameNumber, gameName] = [i + 1, car(game)];
+    result.push(`${gameNumber} – ${gameName}\n`);
+  }
+  result.push('What is the game you want play? ');
+  return result.join('');
+};
 
 export default () => {
   const user = getUserName();
+  const question = makeGameNav(games);
+  const answer = readlineSync.question(question);
 
-  console.log(gameDesc);
-
-  const answer = readlineSync.question('What is the game you want play? ');
   for (let i = 0; i < games.length; i += 1) {
     const game = games[i];
-    const gameNumber = car(game);
-    const gameStart = cdr(game);
-    if (answer === String(gameNumber)) {
-      return gameStart(user);
+    const [gameNumber, gameName, gameFunction] = [i + 1, car(game), cdr(game)];
+    if (answer === String(gameNumber) || answer === gameName) {
+      return gameFunction(user);
     }
   }
 
